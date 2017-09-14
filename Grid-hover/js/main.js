85dfa778712de6cd8e3111474edde5aed980a218
AleksBasko <aleks.spiegel@gmail.com>
2017-09-14 20:10:08 +0300

    Grid hover start

---
+++ Grid-hover/js/main.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    heightGrid();
+    hideHover();
+};
+
+window.addEventListener('resize', function() {
+    heightGrid();
+});
+
+function heightGrid() {
+    let grid = document.querySelector('.grid-zoom');
+    let item = grid.querySelectorAll('.grid-zoom__item');
+
+    for(let i = 0; i < item.length; i++) {
+        let itemWidth = item[i].clientWidth;
+        console.log(itemWidth);
+        item[i].style.height = itemWidth/1.333 + 'px';
+    }
+}
+
+function hideHover() {
+    let grid = document.querySelector('.grid-shad');
+    let item = grid.querySelectorAll('.grid-shad__item');
+
+    for(let i = 0; i < item.length; i++) {
+        let shadow = document.createElement('div');
+        shadow.classList.add('grid-shad__block');
+        item[i].appendChild(shadow);
+
+        item[i].addEventListener('mouseover', function() {
+            for(let j = 0; j < item.length; j++) {
+                if(i !== j) {
+                    item[j].classList.add('grid-shad__item--hide');
+                }
+            }
+        });
+        item[i].addEventListener('mouseout', function() {
+            for(let j = 0; j < item.length; j++) {
+                item[j].classList.remove('grid-shad__item--hide');
+            }
+        });
+    }
+}
