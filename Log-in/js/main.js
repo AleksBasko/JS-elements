3d003d86cd81357a3bed9cddef87aa23ec0c0750
AleksBasko <aleks.spiegel@gmail.com>
2017-09-18 21:20:03 +0300

    Login start preloader

--- Log-in/js/main.js
+++ Log-in/js/main.js
@@ -1,49 +1,49 @@
-
 'use strict';
 
-window.onload = function() {
+window.onload = function () {
     let Register = new LogObj('registr-form');
     Register.init();
+    Register.show();
 };
 
-let LogObj = function(id) {
+let LogObj = function (id) {
     this.form = document.getElementById(id);
     this.inputList = this.form.querySelectorAll('.registr-form__input');
     this.formBtn = this.form.querySelector('.registr-form__btn');
     this.inputCount = 0;
     this.passActive = false;
 
-    this.inputBlur = function() {
+    this.inputBlur = function () {
         let _this = this;
-      let inputList = this.inputList;
-
-      for(let i = 0; i < inputList.length; i++) {
-          inputList[i].addEventListener('focus', function() {
-              let block = this;
-              while (!block.classList.contains('registr-form__row')){
-                  block = block.parentNode;
-              }
-
-              block.classList.add('input-active');
-
-          });
-          inputList[i].addEventListener('blur', function() {
-              _this.empty();
-          });
-      }
-    };
-
-    this.empty = function() {
         let inputList = this.inputList;
 
-        for(let i = 0; i < inputList.length; i++) {
-            if(inputList[i].value) {
+        for (let i = 0; i < inputList.length; i++) {
+            inputList[i].addEventListener('focus', function () {
+                let block = this;
+                while (!block.classList.contains('registr-form__row')) {
+                    block = block.parentNode;
+                }
+
+                block.classList.add('input-active');
+
+            });
+            inputList[i].addEventListener('blur', function () {
+                _this.empty();
+            });
+        }
+    };
+
+    this.empty = function () {
+        let inputList = this.inputList;
+
+        for (let i = 0; i < inputList.length; i++) {
+            if (inputList[i].value) {
                 this.inputCount++;
             }
             else {
                 let block = inputList[i];
 
-                while (!block.classList.contains('registr-form__row')){
+                while (!block.classList.contains('registr-form__row')) {
                     block = block.parentNode;
                 }
 
@@ -51,7 +51,7 @@ let LogObj = function(id) {
             }
         }
 
-        if(this.inputCount === inputList.length) {
+        if (this.inputCount === inputList.length) {
             this.formBtn.removeAttribute('disabled');
         }
         else {
@@ -59,28 +59,30 @@ let LogObj = function(id) {
             this.formBtn.setAttribute("disabled", "disabled");
         }
     };
-
-    this.showPass = function() {
+    this.show = function () {
+        this.form.classList.remove('preloader');
+    };
+    this.showPass = function () {
         let _this = this;
         let passBtn = this.form.querySelector('.registr-form__password-icon');
 
-        passBtn.addEventListener('click', function(){
+        passBtn.addEventListener('click', function () {
 
             let passParent = this;
 
-            while (!passParent.classList.contains('registr-form__password-wrap')){
+            while (!passParent.classList.contains('registr-form__password-wrap')) {
                 passParent = passParent.parentNode;
             }
 
             let passInput = passParent.querySelector('.registr-form__input');
 
-            if(_this.passActive) {
-                passInput.setAttribute('type','password');
+            if (_this.passActive) {
+                passInput.setAttribute('type', 'password');
                 this.classList.remove('registr-form__password-icon--active');
                 _this.passActive = false;
             }
             else {
-                passInput.setAttribute('type','text');
+                passInput.setAttribute('type', 'text');
                 this.classList.add('registr-form__password-icon--active');
                 _this.passActive = true;
             }
@@ -88,7 +90,7 @@ let LogObj = function(id) {
         });
     };
 
-    this.init = function() {
+    this.init = function () {
         this.inputBlur();
         this.showPass();
     }
