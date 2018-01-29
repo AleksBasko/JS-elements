5b4d622abf15f7a189226601e16c32eab4408492
Aleks <aleks.spiegel@gmail.com>
2018-01-29 21:16:47 +0300

    js

--- Word-ending/js/main.js
+++ Word-ending/js/main.js
@@ -1,75 +1,82 @@
 'use strict';
 
-window.onload = function () {
-    let firstTabs = new CreateTabs( 'tab-first' );
-    let secondTabs = new CreateTabs( 'tab-second' );
-    firstTabs.init();
-    secondTabs.init();
+let weInput = document.querySelectorAll('.word-ending__input');
 
-};
+for ( let i=0; i < weInput.length; i++ ) {
+  weInput[i].addEventListener('keydown', function(e) {
+    let keyCode = e.keyCode;
 
-const TAB_BTN = 'tab-btn';
-const TAB_CONT = 'tab-content';
-const TAB_BTN_ACT = 'tab-btn--active';
-const TAB_CONT_ACT = 'tab-content--active';
+    let keySymbol = String.fromCharCode(keyCode);
+    console.log(keySymbol);
+    let rep = /[-\.;":'a-zA-Zа-яА-Я]/;
 
-
-
-let CreateTabs = function(id) {
-    if ( typeof id !== 'string' ) {
-        throw new TypeError( id + 'is not a String' );
+    if (rep.test(keySymbol)) {
+        return false;
     }
+    else {
+      console.log('click');
+        // if()
+        //
+        // $inputRange.val($(this).val()).change();
+        // console.log($(this).val());
 
-    this.block = document.getElementById(id);
-    this.tabBtn = this.block.querySelectorAll('.' + TAB_BTN);
-    this.tabContent = this.block.querySelectorAll('.' + TAB_CONT);
-};
 
-CreateTabs.prototype.init = function () {
-    this.searchActive();
-    this.changeTab();
-};
-
-CreateTabs.prototype.searchActive = function () {
-    let btnArr = this.tabBtn;
-    let contArr = this.tabContent;
-
-    for (let i=0; i < btnArr.length; i++) {
-        if ( btnArr[i].classList.contains( TAB_BTN_ACT ) ){
-            this.activeCount = i;
-        }
+        // setTimeout(function() {
+        //     $(this).val();
+        //     console.log('after ' + $(this).val());
+        //     let $inputRange = $(this).parents('.doctor-filter-settings__input-box').find('input[type="range"]');
+        //     $inputRange.val($(this).val()).change();
+        // }, 20);
+        //
+        // if( $(this).hasClass('doctor-filter-settings__input-years') ) {
+        //     let _this = $(this);
+        //     setTimeout(function() {
+        //         wordEnding(_this);
+        //     }, 20);
+        // }
     }
-    for (let i=0; i < contArr.length; i++) {
-        console.log(contArr[i].offsetHeight);
-        if(i === this.activeCount) {
-            contArr[i].classList.add( TAB_CONT_ACT );
-        }
-    }
-};
+  })
+}
 
-CreateTabs.prototype.changeTab = function () {
-    let _this = this;
-    let btnArr = this.tabBtn;
-    let contArr = this.tabContent;
+// $(".word-ending__input").keydown(function (e) {
+//     let keyCode = e.keyCode;
+//     let keySymbol = String.fromCharCode(keyCode);
+//     let rep = /[-\.;":'a-zA-Zа-яА-Я]/;
+//     if (rep.test(keySymbol)) {
+//         return false;
+//     }
+//     else {
+//         // if()
+//         //
+//         // $inputRange.val($(this).val()).change();
+//         // console.log($(this).val());
+//         setTimeout(function() {
+//             $(this).val();
+//             console.log('after ' + $(this).val());
+//             let $inputRange = $(this).parents('.doctor-filter-settings__input-box').find('input[type="range"]');
+//             $inputRange.val($(this).val()).change();
+//         }, 20);
+//
+//         if( $(this).hasClass('doctor-filter-settings__input-years') ) {
+//             let _this = $(this);
+//             setTimeout(function() {
+//                 wordEnding(_this);
+//             }, 20);
+//         }
+//     }
+// });
 
-    for (let i=0; i < btnArr.length; i++) {
-        btnArr[i].addEventListener( 'click', function () {
-
-            if ( _this.activeCount !== i ) {
-                btnArr[_this.activeCount].classList.remove( TAB_BTN_ACT );
-                this.classList.add( TAB_BTN_ACT );
-
-
-                for ( let j = 0; j < contArr.length; j++ ) {
-                    if(j === _this.activeCount) {
-                        contArr[j].classList.remove( TAB_CONT_ACT );
-                    }
-                    if ( j === i ) {
-                        contArr[j].classList.add( TAB_CONT_ACT );
-                    }
-                }
-                _this.activeCount = i;
-            }
-        } );
-    }
-};
+function wordEnding(block) {
+    // let arr = block.val();
+    // let lastNumber = parseInt(arr[arr.length-1]);
+    // let labelBlock = block.parents('.doctor-filter-settings__input-box').find('.doctor-filter-settings__val');
+    // let labelText = 'лет';
+    // if ( 0 < lastNumber && lastNumber < 2 ) {
+    //     labelText = 'год';
+    // }
+    // else if ( 1 < lastNumber && lastNumber < 5 ) {
+    //     labelText = 'года';
+    // }
+    //
+    // labelBlock.html(labelText);
+}
