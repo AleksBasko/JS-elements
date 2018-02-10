285dc5a81bcc1bc13400aad3e5fb35644c9c4bd9
Aleks <aleks.spiegel@gmail.com>
2018-02-10 22:08:36 +0300

    blink add class

--- Blink-grid/js/script.js
+++ Blink-grid/js/script.js
@@ -1,35 +1,50 @@
-(function() {
-    var grid = document.querySelector('.bg-grid');
 
-    var gridWidth = grid.offsetWidth;
-    var gridHeight = grid.offsetHeight;
+class BlickEffect {
+  constructor(id, size, interval) {
+    this.grid = document.querySelector(id);
+    this.gridItemSize = size || 50;
+    this.interval = interval || 50;
+    this.gridItemCount = null;
 
-    var gridItemSize = 50;
-    var gridItemCount = (gridWidth/gridItemSize) * (gridHeight/gridItemSize);
+    this.init();
+  }
 
-    for (var i = 0; i < gridItemCount; i++) {
+  createGrid() {
+    let gridWidth = this.grid.offsetWidth;
+    let gridHeight = this.grid.offsetHeight;
+    this.gridItemCount = (gridWidth/this.gridItemSize) * (gridHeight/this.gridItemSize);
+
+    for (var i = 0; i < this.gridItemCount; i++) {
         var gridItem = document.createElement('div');
         gridItem.classList.add('bg-grid__item');
-        gridItem.style.width = gridItemSize + 'px';
-        gridItem.style.height = gridItemSize + 'px';
+        gridItem.style.width = this.gridItemSize + 'px';
+        gridItem.style.height = this.gridItemSize + 'px';
 
-        grid.appendChild(gridItem);
+        this.grid.appendChild(gridItem);
     }
+  }
 
-    var blinkItem = document.querySelectorAll('.bg-grid__item');
+  showGrid() {
+    var blinkItem = this.grid.querySelectorAll('.bg-grid__item');
 
-    setInterval(function() {
-        var randValue = getRandomInt(0, gridItemCount);
+    setInterval(()=> {
+        var randValue = this.getRandomInt(0, this.gridItemCount);
         blinkItem[randValue].classList.add('bg-grid__item--blink');
-        console.log(randValue);
-        setTimeout(function(){
+        setTimeout(()=>{
             blinkItem[randValue].classList.remove('bg-grid__item--blink');
         }, 3000)
-    }, 50);
+    }, this.interval);
+  }
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+  getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-}());
+  init() {
+    this.createGrid();
+    this.showGrid();
+  }
+}
 
+let smallGrid = new BlickEffect('.bg-grid--s');
+let largeGrid = new BlickEffect('.bg-grid--l', 100, 200);
